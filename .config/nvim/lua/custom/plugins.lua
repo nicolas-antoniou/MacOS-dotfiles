b4fe2fd69ba484d3b34339b026273324fd4ad1de
nicolas-antoniou <82817704+nicolas-antoniou@users.noreply.github.com>
2024-01-19 17:23:58 +0000

    Create plugins.lua

---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,13 @@
+local plugins = {
+  {
+    "ibhagwan/fzf-lua", lazy = false,
+    -- optional for icon support
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+      -- calling `setup` is optional for customization
+      require("fzf-lua").setup({})
+    end
+  }
+}
+return plugins
+
